b99ca3674f40e5b7afb47c1d593e4cc6c08d5c7c
pacoooh <pacoooh@gmail.com>
2020-11-01 00:42:24 +0100

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,7 +8,7 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import {PDFGenerator} from '@ionic-native/pdf-generator/ngx';
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
@@ -16,6 +16,7 @@ import { AppComponent } from './app.component';
   providers: [
     StatusBar,
     SplashScreen,
+    PDFGenerator,
     { provide: RouteReuseStrategy, useClass: IonicRouteStrategy }
   ],
   bootstrap: [AppComponent]
--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { PDFGenerator } from '@ionic-native/pdf-generator/ngx';
 
 @Component({
   selector: 'app-tab1',
@@ -7,6 +8,111 @@ import { Component } from '@angular/core';
 })
 export class Tab1Page {
 
-  constructor() {}
+  constructor(  private pdfGenerator: PDFGenerator,) {}
 
+  onClick(){
+    
+    let options = {
+      documentSize: 'A4',
+      type: 'share',
+      fileName: 'myFile.pdf'
+      
+    }
+    let div2 = document.getElementById("printable-area").innerHTML;
+
+    let style = ` <style>
+.printable-content {
+  margin: 16px;
+  overflow-x: scroll;
+  overflow-y: scroll;
+  background-color: #ffffff;
+}
+.printable-area {
+  width: 595px;
+  height: 100%;
+}
+.header-table {
+  background-color: #18AC52;
+  margin: 2%;
+  width: 96%;
+  color: #ffffff;
+}
+.header-table th, .header-table td {
+  text-align: left;
+  padding: 5px;
+}
+.header-table .title {
+  padding: 5px 20px;
+  font-size: 20px;
+  font-weight: bold;
+  text-transform: uppercase;
+}
+h1, h3 {
+  margin-left: 10px;
+}
+h3 {
+  margin-top: 20px;
+}
+.subheader-table {
+  margin: 2%;
+  width: 96%;
+}
+.subheader-table td {
+  vertical-align: top;
+}
+.subheader-table td dt {
+  font-size: 11px;
+  color: #999;
+  margin-bottom: 5px;
+}
+ subheader-table td dd {
+  margin: 0;
+}
+.subheader-table td:last-child {
+  text-align: right;
+ }
+.subheader-table td:last-child dd {
+  font-size: 28px;
+  font-weight: bold;
+  }     
+.detail-table {
+  margin: 2%;
+  width: 96%;
+  border-top: 2px solid #CCC;
+  border-bottom: 2px solid #CCC;
+}
+.detail-table th,.detail-table td {
+    padding: 5px;
+}
+.detail-table th {
+  border-bottom: 2px solid #CCC;
+} 
+.detail-table td  { 
+  text-align: right;
+}
+.detail-table td:first-child {
+  text-align: left;
+}
+.footer-table {
+  margin: 2%;
+  width: 96%;
+}
+.footer-table td {
+  padding: 5px;
+  text-align: right;
+  font-weight: bold;
+}
+        
+    </style>`
+
+
+    this.pdfGenerator.fromData( style+ div2 ,options)
+    .then((b)=>{
+      
+         })
+      .catch((err)=>console.log(err))
+
+ 
+  }  
+ 
 }
